90ec234790737c7a8c54d68ede720b6cbb6afba1
Ryan Coleman <coleman.ryan@gmail.com>
2026-05-16 12:15:20 -0400

    test(07-00): add server_auth_test.go with 9 stub functions
    - TestNew_NoAuthMechanism, TestNew_NoTLS, TestAPIKey_Valid, TestAPIKey_Missing
    - TestMTLS_NoCert, TestAtomicKeySet, TestAtomicKeyReload
    - TestConfigHolder_ReloadValidation, TestConnRegistry_RemoveOnEnd
    - All stubs call t.Skip(); compile and skip cleanly under go test

---
+++ api/grpc/server/server_auth_test.go
@@ -0,0 +1,39 @@
+package server
+
+import "testing"
+
+func TestNew_NoAuthMechanism(t *testing.T) {
+	t.Skip("stub -- implemented in Plan 06")
+}
+
+func TestNew_NoTLS(t *testing.T) {
+	t.Skip("stub -- implemented in Plan 06")
+}
+
+func TestAPIKey_Valid(t *testing.T) {
+	t.Skip("stub -- implemented in Plan 06")
+}
+
+func TestAPIKey_Missing(t *testing.T) {
+	t.Skip("stub -- implemented in Plan 06")
+}
+
+func TestMTLS_NoCert(t *testing.T) {
+	t.Skip("stub -- implemented in Plan 06")
+}
+
+func TestAtomicKeySet(t *testing.T) {
+	t.Skip("stub -- implemented in Plan 06")
+}
+
+func TestAtomicKeyReload(t *testing.T) {
+	t.Skip("stub -- implemented in Plan 06")
+}
+
+func TestConfigHolder_ReloadValidation(t *testing.T) {
+	t.Skip("stub -- implemented in Plan 06")
+}
+
+func TestConnRegistry_RemoveOnEnd(t *testing.T) {
+	t.Skip("stub -- implemented in Plan 06")
+}
